8d42d97ca9e0cdc83b2801148b1d2607c051ad16
ccoignard2021 <ccoignard2021@172SE12-91HMX53>
2021-04-08 15:34:14 +0200

    commition spéculative

--- src/fr/eni/demoDePoo/Operation.java
+++ src/fr/eni/demoDePoo/Operation.java
@@ -1,10 +1,65 @@
 package fr.eni.demoDePoo;
 
 public class Operation {
-	
+
 	private String retrait;
 	private String depot;
 	private String decouvert;
-	private String virement
+	private String virement;
+
+	public Operation(String retrait, String depot, String decouvert, String virement) {
+		super();
+		this.retrait = retrait;
+		this.depot = depot;
+		this.decouvert = decouvert;
+		this.virement = virement;
+	}
+
+	public String getRetrait() {
+		return retrait;
+	}
+
+	public void setRetrait(String retrait) {
+		this.retrait = retrait;
+	}
+
+	public String getDepot() {
+		return depot;
+	}
+
+	public void setDepot(String depot) {
+		this.depot = depot;
+	}
+
+	public String getDecouvert() {
+		return decouvert;
+	}
+
+	public void setDecouvert(String decouvert) {
+		this.decouvert = decouvert;
+	}
+
+	public String getVirement() {
+		return virement;
+	}
+
+	public void setVirement(String virement) {
+		this.virement = virement;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Operation [retrait=");
+		builder.append(retrait);
+		builder.append(", depot=");
+		builder.append(depot);
+		builder.append(", decouvert=");
+		builder.append(decouvert);
+		builder.append(", virement=");
+		builder.append(virement);
+		builder.append("]");
+		return builder.toString();
+	}
 
 }
